c78c93a395afba01668aba3dd2a90304eaaf20ab
scduffy <scduffy@loyola.edu>
2017-09-05 22:32:34 -0400

    added some stuff

--- test.py
+++ test.py
@@ -2,5 +2,14 @@ def out():
     x = input('what do you want to say: ')
     return x
 
-print(out())
+def add(x,y):
+    z = x + y
+    return z
+
+def subtract(x,y):
+    z = x - y
+    return z
+
+print(add(9,1))
+
 
